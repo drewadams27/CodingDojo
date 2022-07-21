262f91d5a78b81285c06d31493211e52cf4c6c3a
Drew Adams <drewadams27@gmail.com>
2022-07-21 11:59:54 -0700

    userswithbankaccounts assignment

---
+++ userswithbankaccounts.py
@@ -0,0 +1,54 @@
+from unicodedata import name
+
+
+class BankAccount:
+    accounts = []
+    def __init__(self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        if (self.balance - amount) >= 0:
+            self.balance -= amount
+        else:
+            print("Insufficient Funds: Charging a $5 fee.")
+            self.balance -= 5
+        return self
+
+    def display_account_info(self):
+        print(f"Balance: {self.balance}")
+        return self
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate)
+        return self
+
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(0.2, 1000)
+
+    def make_deposit(self, amount):
+        self.account.deposit(amount)
+        return self
+
+    def make_withdrawal(self, amount):
+        self.account.withdraw(amount)
+        return self
+
+    def display_user_balance(self):
+        print(self.account.balance)
+
+
+drew = User("Drew", "address@mailsite.com")
+
+drew.make_deposit(1234)
+drew.make_withdrawal(1000)
+drew.display_user_balance()
+
